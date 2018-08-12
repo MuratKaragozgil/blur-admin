ad24564d0b8d10b6eebc4666f4a0e32459e7d50e
Murat Karagozgil <murat.karagozgil@inomera.com>
2018-08-13 00:14:36 +0300

    Added login endpoint to sign in page

--- src/app/pages/authSignIn/authSignInCtrl.js
+++ src/app/pages/authSignIn/authSignInCtrl.js
@@ -1,31 +1,47 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular.module('BlurAdmin.pages.authSignIn')
-    .controller('authSignInCtrl', authSignInCtrl);
+    angular.module('BlurAdmin.pages.authSignIn')
+        .controller('authSignInCtrl', authSignInCtrl);
 
-  /** @ngInject */
-  function authSignInCtrl($scope, localStorage, $state) {
-    var vm = this;
+    /** @ngInject */
+    function authSignInCtrl($scope, localStorage, $state, $http) {
+        var vm = this;
 
-    vm.logar = logar;
+        vm.logar = logar;
 
-    init();
+        init();
 
-    function init() {
-      localStorage.clear();
+        function init() {
+            localStorage.clear();
+        }
+
+        // TODO cors origin problem will be fixed with adding some header
+        var config = {
+            headers: {
+                'Authorization': 'Basic d2VudHdvcnRobWFuOkNoYW5nZV9tZQ==',
+                'Accept': 'application/json;odata=verbose',
+                "Access-Control-Allow-Origin": "*",
+                "X-Testing": "testing"
+            }
+        };
+
+        $http.post("http://localhost:9090/auth/login", config).then(function (result) {
+            console.log(result);
+        }, function (result) {
+            console.log(result);
+        });
+
+        function logar() {
+            var dadosUser = {
+                user: vm.user,
+                passWord: vm.passWord
+            };
+            localStorage.setObject('dataUser', dadosUser);
+            $state.go('main.dashboard');
+        }
+
+
     }
-
-    function logar() {
-      var dadosUser = {
-        user: vm.user,
-        passWord: vm.passWord
-      };
-      localStorage.setObject('dataUser', dadosUser);
-      $state.go('main.dashboard');
-    }
-
-
-  }
 
 })();
